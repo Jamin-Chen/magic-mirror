a932e491c5a8a834312759a8eaf3ac9e474a67ba
Jamin Chen <jamin.chen587@gmail.com>
2016-12-04 21:05:10 -0800

    Added MAKERS temp logo, added github commit hash script (not sure if its working rn)

--- script.js
+++ script.js
@@ -30,3 +30,18 @@
     }
     startTime();
 })();
+
+(function checkVersion()
+{
+    $.getJSON('githash.php', {}, function(json, textStatus) {
+        if (json) {
+            if (json.gitHash != gitHash) {
+                window.location.reload();
+                window.location.href=window.location.href;
+            }
+        }
+    });
+    setTimeout(function() {
+        checkVersion();
+    }, 3000);
+})();
