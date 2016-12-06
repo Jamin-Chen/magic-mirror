0193fdb1c46baa3ac535a578669c82079febab00
Jamin Chen <jamin.chen587@gmail.com>
2016-12-05 22:45:35 -0800

    moved files around, shrunk weather widget; still trying to fix the weather icon display on the mirror

--- weather.js
+++ weather.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     unit: 'f',
     success: function(weather) {
       html = '<temperature><icon class="icon-'+weather.code+'"></icon> '+weather.temp+'&deg;'+weather.units.temp+'</temperature>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<location>'+weather.city+', '+weather.region+'</location>';
+      html += '<description class="currently">'+weather.currently+'</description>';
 
       $("#weather").html(html);
     },
